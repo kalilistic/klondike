9037ea07219c34330491b0683ac2412c6dfa51c1
kalilistic <35899782+kalilistic@users.noreply.github.com>
2020-07-02 18:41:44 -0400

    update change log

--- src/constants/Splash.js
+++ src/constants/Splash.js
@@ -1,6 +1,10 @@
 let SplashMessage = {
-  version: 2,
-  title: "Something new.",
-  messages: ["- Change your own bar color."]
+  version: 3,
+  title: "happy july!",
+  messages: [
+    "- Added new option (under player details) to add deaths, heal%, etc. right to your overlay.",
+    "- Added new settings screen called player details.",
+    "- Improved Chinese translations (thanks ShadyWhite)."
+  ]
 };
 export default SplashMessage;
