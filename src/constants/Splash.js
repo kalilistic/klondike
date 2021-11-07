cd35af6b788c832d017212ca3e64880e7e7d79bd
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-11-07 18:58:54 -0500

    chore: update change notes

--- src/constants/Splash.js
+++ src/constants/Splash.js
@@ -1,6 +1,11 @@
 let SplashMessage = {
-  version: 5,
-  title: "new stat",
-  messages: ["added hps to player popup per request"]
+  version: 6,
+  title: "enjoy expac launch!",
+  messages: [
+    "- feat: select MaxHit as secondary stat.",
+    "- fix: select HPS as secondary stat.",
+    "- fix: marauder shows correctly as blue (tank).",
+    "- chore: easier updates with gh actions."
+  ]
 };
 export default SplashMessage;
